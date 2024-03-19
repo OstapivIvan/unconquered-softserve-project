c07bc2cd1a0692a9a714253fd4b5299333a46925
MaPo00 <uraboi4enko18@gmail.com>
2024-03-19 14:59:40 +0200

    Add Navigation Bar

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -5,20 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        MAIN = this
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+           when(it.itemId){
+               R.id.home -> replaceFragment(Home())
+               R.id.categories -> replaceFragment(Categories())
+               R.id.settings -> replaceFragment(Settings())
+               R.id.profile -> replaceFragment(Profile())
+
+               else -> {
+
+               }
+           }
+            true
+        }
+
+
+    }
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
 
 
     }
